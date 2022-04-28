2c1b2046148dba18877f1f1c9dd9c74af2a1e460
saurabh-gaonkar <saurabhg@axioned.com>
2022-04-28 12:03:33 +0530

    js variables declared

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,58 @@
-/* Author: 
+let questions = [
+    {"question":"Who invented the BALLPOINT PEN?",
+        "options": ["Biro Brothers","Waterman Brothers","Bicc Brothers","Write Brothers"],
+        "answer": "Biro Brothers"
+    },
+    {"question":"In which decade was the first solid state integrated circuit demonstrated?",
+        "options": ["1950s","1960s","1970s","1980s"],
+        "answer": "1950s"
+    },
+    {"question":"What J. B. Dunlop invented?",
+        "options": ["Automobile wheel rim","Pneumatic rubber tire","Rubber boot","Model airplanes"],
+        "answer": "Pneumatic rubber tire"
+    },
+    {"question":"Which scientist discovered the radioactive element radium?",
+        "options": ["Isaac Newton","Albert Einstein","Benjamin Franklin","Marie Curie"],
+        "answer": "Marie Curie"
+    },
+    {"question":"The metal whose salts are sensitive to light is",
+        "options": ["Zinc","Silver","Copper","Aluminum"],
+        "answer": "Silver"
+    },
+    {"question":"	When was barb wire patented?",
+        "options": ["1874","1840","1895","1900"],
+        "answer": "1874"
+    },
+    {"question":"What Galileo invented?",
+        "options": ["Barometer","Pendulum clock","Microscope","Thermometer"],
+        "answer": "Thermometer"
+    },
+    {"question":"Tsunamis are not caused by",
+    "options": ["Hurricanes","Earthquakes","Undersea landslides","Volcanic eruptions"],
+    "answer": "Hurricanes"
+    },
 
-*/
+    {"question":"Who invented Jet Engine?",
+        "options": ["Sir Frank Whittle","Gottlieb Daimler","Roger Bacon","	Lewis E. Waterman"],
+        "answer": "Sir Frank Whittle"
+    },
+    {"question":"What invention caused many deaths while testing it?",
+        "options": ["Dynamite","Ladders","Race cars","Parachute"],
+        "answer": "Parachute"
+    },
+  
+];
 
+let answers = [];
 
-
-
-
-
+const start = document.querySelector(".start_btn"),
+submit = document.querySelector(".submit_btn"),
+previous = document.querySelector(".previous_btn"),
+next = document.querySelector(".next_btn"),
+back = document.querySelector(".back_btn"),
+question_box = document.querySelector(".question_box"),
+question_container = document.querySelector(".questions"),
+result = document.querySelector(".result_box");
 
 
 
